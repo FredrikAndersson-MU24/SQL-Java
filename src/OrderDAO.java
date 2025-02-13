cd6074da1e708ef90ec098b4a71d924e8a222c2e
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-02-13 14:04:26 +0100

    Insert a new order using a transaction

--- src/OrderDAO.java
+++ src/OrderDAO.java
@@ -75,4 +75,48 @@ public class OrderDAO {
         }
         return orderHistory;
     }
+
+
+    public void insertOrder(int customerId, List<OrderDetail> orderDetailsList) {
+        Connection conn = Database.getConnection();
+
+        try {
+            conn.setAutoCommit(false);
+
+            String insertOrderQuery = "INSERT INTO orders (customer_id) VALUES (?);";
+            PreparedStatement stmt = conn.prepareStatement(insertOrderQuery, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, customerId);
+            stmt.executeUpdate();
+            ResultSet keySet = stmt.getGeneratedKeys();
+            int orderId = 0;
+            if (keySet.next()) {
+                orderId = keySet.getInt(1);
+            }
+
+            String orderDetailQuery = "INSERT INTO order_details (order_id, product_id, quantity) VALUES (?, ?, ?);";
+            for (OrderDetail od : orderDetailsList) {
+                PreparedStatement detailStatement = conn.prepareStatement(orderDetailQuery);
+                detailStatement.setInt(1, orderId);
+                detailStatement.setInt(2, od.getProduct_id());
+                detailStatement.setInt(3, od.getQuantity());
+                detailStatement.executeUpdate();
+            }
+
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println("FAILED");
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } finally {
+            try {
+                conn.setAutoCommit(true);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
